b8947aaa92caa25cdcd22e3c95fb971dd858de6a
SunJay <189344026+tech-Enthusiast7@users.noreply.github.com>
2026-03-11 06:06:53 +0000

    Comparison added

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,22 @@
+// console.log("2" > 1);
+// console.log("02" >= 1);
+
+
+// Comparison operator convert the null to 0 but not the equaltiy opeartor
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null < 0);
+// console.log(null <= 0);
+
+
+console.log("undefined");
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+console.log("Strict Checking");
+console.log("2" === 2);
+
+
