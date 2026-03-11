5cabae66babee21098311716b9b5fcfe3f877f96
SunJay <189344026+tech-Enthusiast7@users.noreply.github.com>
2026-03-11 03:46:29 +0000

    modify datatype files

--- 01_Basics/02_DataTypes.js
+++ 01_Basics/02_DataTypes.js
@@ -5,3 +5,23 @@
 
 console.log(3 
     + 3);           // Code Readability Should be Maintained
+
+
+let name = "Sanjay";
+let age = 18;
+let isLoggedIn = false;
+
+
+// number => 2 ^ 53
+// bigInt
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined => Not yet assigned
+// symbol => unique
+
+
+//Object
+console.log(typeof null);       // Error : Output - Object
+console.log(typeof age);        
+console.log(typeof undefined);  
