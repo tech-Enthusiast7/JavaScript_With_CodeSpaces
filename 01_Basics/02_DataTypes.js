a84eb148b910c30c049cfac30c182495b2fd59d5
SunJay <189344026+tech-Enthusiast7@users.noreply.github.com>
2026-03-11 03:42:23 +0000

    Add Datatypes file

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,7 @@
+"use strict"; // Treat all JS code as newer Version
+
+
+// alert(3 + 3);            // We are using node.js, not browser
+
+console.log(3 
+    + 3);           // Code Readability Should be Maintained
