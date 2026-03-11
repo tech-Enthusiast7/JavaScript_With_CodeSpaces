ec28053e206d3d5634e4a52ea7fce0cafcebe5a1
SunJay <189344026+tech-Enthusiast7@users.noreply.github.com>
2026-03-11 05:58:14 +0000

    Operations in the file

--- 01_Basics/03_Conversion_n_Operators.js
+++ 01_Basics/03_Conversion_n_Operators.js
@@ -42,3 +42,27 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// ************************** Operations ******************************
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+
+let str1 = "Sun";
+let str2 = "Zay";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1"  + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+
+
+
