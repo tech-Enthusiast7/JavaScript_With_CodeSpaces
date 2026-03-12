35744ae7e20d047b1dd094edee8ff0120e6e21d7
SunJay <189344026+tech-Enthusiast7@users.noreply.github.com>
2026-03-12 06:51:04 +0000

    Function with object and array

--- 03_Basics/01_Function.js
+++ 03_Basics/01_Function.js
@@ -44,3 +44,34 @@ function loginUserMessage(username = "User") {
 // loginUserMessage("hitesh");
 // console.log(loginUserMessage(""));
 
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1;
+}
+
+// console.log(calculateCartPrice(200, 400, 500));
+
+const user = {
+    username: "hitesh",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is {anyobject.price}`);
+    
+}
+
+// handleObject(user);
+
+handleObject({
+    username: "Sam",
+    price: 300
+});
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+    return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([500, 200, 600]));
