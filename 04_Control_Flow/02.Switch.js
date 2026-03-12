bfaa7d81a79cc2ba66287c2c3be3c09f9ed4956e
SunJay <189344026+tech-Enthusiast7@users.noreply.github.com>
2026-03-12 13:48:19 +0000

    Switch Statments Explained

---
+++ 04_Control_Flow/02.Switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3;
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+            
+        break;
+    case 2:
+        console.log("feb");
+            
+        break;
+    case 3:
+        console.log("mar");
+            
+        break;
+    case 4:
+        console.log("apr");
+            
+        break;
+
+    default:
+        break;
+}
