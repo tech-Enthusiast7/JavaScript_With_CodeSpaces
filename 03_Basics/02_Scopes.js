cbd0e3f0cf7483855d374b6bbd489abfd002deb6
SunJay <189344026+tech-Enthusiast7@users.noreply.github.com>
2026-03-12 07:04:38 +0000

    Scope and Hoisting

--- 03_Basics/02_Scopes.js
+++ 03_Basics/02_Scopes.js
@@ -15,4 +15,41 @@ if(true) {
 
 
 
- 
+ function one() {
+    const username = "Hitesh";
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    two();
+    
+ }
+//  one();
+
+if(true) {
+    const username = "hitesh";
+    if(username === "hitesh") {
+        const website = " youtube";
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// +++++++++++++ INTRESTING Q. +++++++++++++++++
+
+
+// addone(5);
+
+function addone(num) {
+    return num + 1;
+}
+
+//addTwo(5)
+
+const addTwo = function(num) {
+    return num + 2;
+}
